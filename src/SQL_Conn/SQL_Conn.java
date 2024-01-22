27aca3416a8ebacf538d332f397ae91c96d80ff1
Aaron <Aaron@DESKTOP-AHN9B7K>
2024-01-22 21:42:15 +0800

    Check it out if I'm still sane

--- src/SQL_Conn/SQL_Conn.java
+++ src/SQL_Conn/SQL_Conn.java
@@ -1,9 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package SQL_Conn;
+import java.sql.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class SQL_Conn {
+    
+    static final String user = "root";
+    static final String pass = "";
+    static final String connector = "";
+    
+    PreparedStatement db_ps = null;
     
 }
